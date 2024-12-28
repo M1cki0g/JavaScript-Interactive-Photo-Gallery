6944d8320a206be515e9dddff4ceac71201e4dc5
ToulAura <102886021+M1cki0g@users.noreply.github.com>
2024-12-28 22:30:12 +0100

    Create sol.js

---
+++ sol.js
@@ -0,0 +1,18 @@
+function display1(element) {
+  document.getElementById("main").style.backgroundImage =
+    "url('télécharger.jpeg')";
+  document.getElementById("main").style.backgroundSize = "cover";
+  document.getElementById("text").innerHTML = element.alt;
+}
+function display2(element) {
+  document.getElementById("main").style.backgroundImage =
+    "url('télécharger (1).jpeg')";
+  document.getElementById("main").style.backgroundSize = "cover";
+  document.getElementById("text").innerHTML = element.alt;
+}
+function display3(element) {
+  document.getElementById("main").style.backgroundImage =
+    "url('télécharger (2).jpeg')";
+  document.getElementById("main").style.backgroundSize = "cover";
+  document.getElementById("text").innerHTML = element.alt;
+}
